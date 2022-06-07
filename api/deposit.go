fe92d3c7adbbe3981f9406e29baf0aa8eb2cb139
jiangjie <670884108@qq.com>
2022-06-07 11:07:08 +0800

    fix version_test.go

--- api/deposit.go
+++ api/deposit.go
@@ -221,7 +221,7 @@ func (s *Server) ExistDepositConds(ctx context.Context, in *npool.ExistDepositCo
 
 	if len(conds) == 0 {
 		logger.Sugar().Errorf("empty deposit fields: %v", err)
-		return &npool.ExistDepositCondsResponse{}, status.Error(codes.Internal, "empty stock fields")
+		return &npool.ExistDepositCondsResponse{}, status.Error(codes.Internal, "empty deposit fields")
 	}
 
 	schema, err := crud.New(ctx, nil)
