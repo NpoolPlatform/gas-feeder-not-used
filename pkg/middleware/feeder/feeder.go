2aa85b792a1f1d9c036d84fa7b42cdc5d054b296
Zhao KK <kikakkz@hotmailcom>
2022-06-07 09:42:52 +0000

    Add comment

--- pkg/middleware/feeder/feeder.go
+++ pkg/middleware/feeder/feeder.go
@@ -5,5 +5,11 @@ import (
 )
 
 func Run(ctx context.Context) error {
+	// Get coins
+	// Get payment address
+	// Get coin gas
+	// Check balance
+	// If low than threshold, wait gas account release, lock gas account, create transaction
+	// Record to deposit
 	return nil
 }
