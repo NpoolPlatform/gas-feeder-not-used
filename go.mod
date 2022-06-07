06c315a45135b6fda6c3e3a62f64cac64c5d00e6
Zhao KK <kikakkz@hotmailcom>
2022-06-07 11:34:25 +0000

    Add feeder

--- go.mod
+++ go.mod
@@ -4,23 +4,25 @@ go 1.17
 
 require (
 	entgo.io/ent v0.10.1
-	github.com/NpoolPlatform/api-manager v0.0.0-20220121051827-18c807c114dc
-	github.com/NpoolPlatform/go-service-framework v0.0.0-20220120091626-4e8035637592
+	github.com/NpoolPlatform/api-manager v0.0.0-20220328101926-8907b2f76c6d
+	github.com/NpoolPlatform/go-service-framework v0.0.0-20220404143809-82c40930388a
 	github.com/NpoolPlatform/libent-cruder v0.0.0-20220526050249-956b54fac9f1
 	github.com/NpoolPlatform/message v0.0.0-20220607091058-c3ab98a45e05
+	github.com/NpoolPlatform/sphinx-coininfo v0.0.0-20220607105540-81705a8baf95
 	github.com/go-resty/resty/v2 v2.7.0
 	github.com/google/uuid v1.3.0
-	github.com/grpc-ecosystem/grpc-gateway/v2 v2.8.0
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.10.0
 	github.com/streadway/amqp v1.0.0
-	github.com/stretchr/testify v1.7.1-0.20210427113832-6241f9ab9942
-	github.com/urfave/cli/v2 v2.3.0
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
+	github.com/stretchr/testify v1.7.1
+	github.com/urfave/cli/v2 v2.4.0
+	golang.org/x/xerrors v0.0.0-20220411194840-2f41105eb62f
 	google.golang.org/grpc v1.46.2
 	google.golang.org/protobuf v1.28.0
 )
 
 require (
 	ariga.io/atlas v0.3.7-0.20220303204946-787354f533c3 // indirect
+	github.com/NpoolPlatform/cloud-hashing-billing v0.0.0-20220607111006-17e4f23d8040 // indirect
 	github.com/Shonminh/apollo-client v0.4.0 // indirect
 	github.com/agext/levenshtein v1.2.1 // indirect
 	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
@@ -86,7 +88,7 @@ require (
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
 	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
 	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/genproto v0.0.0-20220302033224-9aa15565e42a // indirect
+	google.golang.org/genproto v0.0.0-20220317150908-0efb43f6373e // indirect
 	gopkg.in/ini.v1 v1.66.2 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
--- pkg/middleware/feeder/feeder.go
+++ pkg/middleware/feeder/feeder.go
@@ -2,12 +2,78 @@ package feeder
 
 import (
 	"context"
+	"time"
+
+	logger "github.com/NpoolPlatform/go-service-framework/pkg/logger"
+	cruder "github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+
+	billingcli "github.com/NpoolPlatform/cloud-hashing-billing/pkg/client"
+	billingpb "github.com/NpoolPlatform/message/npool/cloud-hashing-billing"
+
+	coininfopb "github.com/NpoolPlatform/message/npool/coininfo"
+	coininfocli "github.com/NpoolPlatform/sphinx-coininfo/pkg/client"
+
+	coingascrud "github.com/NpoolPlatform/gas-feeder/pkg/crud/coingas"
+	npool "github.com/NpoolPlatform/message/npool/gasfeeder"
 )
 
+func withCRUD(ctx context.Context, fn func(schema *coingascrud.CoinGas) error) error {
+	schema, err := coingascrud.New(ctx, nil)
+	if err != nil {
+		return err
+	}
+	return fn(schema)
+}
+
+type Feeder struct {
+	coins    []*coininfopb.CoinInfo
+	accounts []*billingpb.GoodPayment
+	gases    []*npool.CoinGas
+}
+
+func (f *Feeder) FeedAll(ctx context.Context) error {
+	return nil
+}
+
 func Run(ctx context.Context) error {
-	// Get coins
-	// Get payment address
-	// Get coin gas
+	ticker := time.NewTicker(12 * time.Hour)
+	ctx, cancel := context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+
+	for range ticker.C {
+		coins, err := coininfocli.GetCoinInfos(ctx, cruder.NewFilterConds())
+		if err != nil {
+			logger.Sugar().Errorf("fail get coininfos: %v", err)
+			continue
+		}
+
+		accounts, err := billingcli.GetGoodPayments(ctx, cruder.NewFilterConds())
+		if err != nil {
+			logger.Sugar().Errorf("fail get good payments: %v", err)
+			continue
+		}
+
+		gases := []*npool.CoinGas{}
+		err = withCRUD(ctx, func(schema *coingascrud.CoinGas) error {
+			gases, _, err = schema.Rows(ctx, cruder.NewConds(), 0, 0)
+			return err
+		})
+		if err != nil {
+			logger.Sugar().Errorf("fail get coin gases: %v", err)
+			continue
+		}
+
+		_feeder := &Feeder{
+			coins:    coins,
+			accounts: accounts,
+			gases:    gases,
+		}
+		err = _feeder.FeedAll(ctx)
+		if err != nil {
+			logger.Sugar().Errorf("fail feed gases: %v", err)
+		}
+	}
+
 	// Check balance
 	// If low than threshold, wait gas account release, lock gas account, create transaction
 	// Record to deposit
